d56a14ce438e05f5484da339f84d764e5e805951
David9945 <dmacaskill19@gmail.com>
2025-08-01 22:58:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const nav = document.getElementById('nav')
 
 burger.addEventListener("click", () => {
     nav.classList.toggle('clicked');
-    burger.textContent = burger.textContent === '☰' ? 'X' : '☰'; // '?' means yes and ':' means no
+    burger.textContent = burger.textContent === '☰' ? '✖' : '☰'; // '?' means yes and ':' means no
 })
+
